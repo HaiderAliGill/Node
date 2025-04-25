4b6cc88e33f776b2accc76f617c97d6e68faafef
HaiderAliGill <haidergill09876@gmail.com>
2025-04-24 20:38:42 -0700

    set Web server

--- Web-server.js
+++ Web-server.js
@@ -1,10 +1,16 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Haider');
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello Haider G</h1>");
+  }
+  if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>About Page</h1>");
+  }
 });
 
 server.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+  console.log("Server is running at http://localhost:3000");
 });
